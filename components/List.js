53e7d6396ab8c4cc3702679f7506115bb5e5d20f
anniehuynh <anniehuynh253@gmail.com>
2022-01-12 11:04:42 +0200

    changes made to fetch real APT, http-b ready

--- components/List.js
+++ components/List.js
@@ -1,32 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
+import {apiUrl, uploadsUrl} from '../utils/variables';
 import ListItem from './ListItem';
-
-const url =
-  'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
 
 const List = () => {
   const [mediaArray, setMediaArray] = useState([]);
-  const loadMedia = async () => {
+  const loadMedia = async (start = 0, limit = 10) => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(
+        `${apiUrl}media?start=${start}&limit=${limit}`
+      );
       if (!response.ok) {
         throw Error(response.statusText);
       }
       const json = await response.json();
-      setMediaArray(json);
+      const media = await Promise.all(
+        json.map(async (item) => {
+          const response = await fetch(apiUrl + 'media/' + item.file_id);
+          const mediaData = await response.json();
+          return mediaData;
+        })
+      );
+      setMediaArray(media);
       console.log(mediaArray);
     } catch (error) {
       console.log(error);
     }
   };
+  //useEffect to call loadMedia() once instead of looping it when the List component is loaded
   useEffect(() => {
-    loadMedia();
+    loadMedia(0, 5);
   }, []);
   return (
     <FlatList
       data={mediaArray}
-      keyExtractor={(item) => item.title}
+      keyExtractor={(item) => item.file_id.toString()}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     ></FlatList>
   );
