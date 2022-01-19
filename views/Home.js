e507b73bb8fdfc580cd3206920ea98ebf69ae396
anniehuynh <anniehuynh253@gmail.com>
2022-01-19 10:55:55 +0200

    add navigation to Single page, click listener to list items, fetch and show single item in Single

--- views/Home.js
+++ views/Home.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import GlobalStyles from '../utils/GlobalStyles';
 import List from '../components/List';
+import PropTypes from 'prop-types';
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <SafeAreaView style={GlobalStyles.AndroidSafeArea}>
-      <List />
+      <List navigation={navigation} />
     </SafeAreaView>
   );
 };
 
+Home.propTypes = {
+  navigation: PropTypes.object,
+};
 export default Home;
