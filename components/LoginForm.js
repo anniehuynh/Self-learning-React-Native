76299a50f7d869b63d998ba5dc2d664cfd25c680
anniehuynh <anniehuynh253@gmail.com>
2022-01-20 11:59:00 +0200

    add placeholder to form fields

---
+++ components/LoginForm.js
@@ -0,0 +1,81 @@
+import React, {useContext} from 'react';
+import {Text, View, TextInput, Button} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import {useForm, Controller} from 'react-hook-form';
+import {MainContext} from '../contexts/MainContext';
+import {useLogin} from '../hooks/ApiHooks';
+
+const LoginForm = () => {
+  const {setIsLoggedIn} = useContext(MainContext);
+  const {postLogin} = useLogin();
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    try {
+      const userData = await postLogin(data);
+      if (userData) {
+        await AsyncStorage.setItem('userToken', userData.token);
+        setIsLoggedIn(true);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <View>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            style={{borderWidth: 1}}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            autoCapitalize="none"
+            placeholder="Username"
+          />
+        )}
+        name="username"
+      />
+      {errors.username && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            style={{borderWidth: 1}}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            autoCapitalize="none"
+            placeholder="Username"
+          />
+        )}
+        name="password"
+      />
+      {errors.password && <Text>This is required.</Text>}
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+    </View>
+  );
+};
+
+export default LoginForm;
