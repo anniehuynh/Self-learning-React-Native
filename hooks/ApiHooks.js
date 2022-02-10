f7f92c166d61c68fff108bc17717f959926a1de9
anniehuynh <anniehuynh253@gmail.com>
2022-02-10 09:29:04 +0200

    like, update like count

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -25,14 +25,14 @@ const useMedia = () => {
   const {update} = useContext(MainContext);
   const loadMedia = async (start = 0, limit = 10) => {
     try {
-      const response = await fetch(
-        `${apiUrl}media?start=${start}&limit=${limit}`
-      );
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      const json = await response.json();
-      // const json = await useTag().getFilesByTag(appId);
+      // const response = await fetch(
+      //   `${apiUrl}media?start=${start}&limit=${limit}`
+      // );
+      // if (!response.ok) {
+      //   throw Error(response.statusText);
+      // }
+      // const json = await response.json();
+      const json = await useTag().getFilesByTag(appId);
       const media = await Promise.all(
         json.map(async (item) => {
           const response = await fetch(apiUrl + 'media/' + item.file_id);
