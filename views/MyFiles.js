a132442d2f96118dd241d23d44439991a4924e1b
anniehuynh <anniehuynh253@gmail.com>
2022-02-10 11:53:05 +0200

    myfile view

--- views/MyFiles.js
+++ views/MyFiles.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import {Layout, Text, Card} from '@ui-kitten/components';
-import {ScrollView} from 'react-native';
+
 import PropTypes from 'prop-types';
 import List from '../components/List';
 
 const MyFiles = ({navigation}) => {
-  return (
-    <List navigation={navigation} myFilesOnly="true" />
-    // <Layout>
-    //   <Text>MyFiles</Text>
-    //   <Card>
-    //     <ScrollView>
-
-    //     </ScrollView>
-    //   </Card>
-    // </Layout>
-  );
+  return <List navigation={navigation} myFilesOnly="true" />;
 };
 
 MyFiles.propTypes = {
